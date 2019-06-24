366ab4944f85806fb32e85d2d5f7d4905a9d6d01
pim.bohm <pim.bohm@gmail.com>
2019-06-24 10:33:30 +0200

    123

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,8 +12,8 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+            return $this->render('default/index.html.twig', [
+                'controller_name' => 'DefaultController',
+            ]);
     }
 }
